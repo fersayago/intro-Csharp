1629d8c36556c3090107fd64834ecd4604588410
fersayago <sayago.fernando@gmail.com>
2021-12-30 18:03:48 -0300

    intial stage

---
+++ StringExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write fullname: ");
+            string fullUserName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {fullUserName}, welcome to Platzi");
+        }
+    }
+}
