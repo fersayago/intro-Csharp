5969a4254105e29b542e6289382b802aeb5a8b01
fersayago <sayago.fernando@gmail.com>
2021-12-30 18:11:47 -0300

    variables de texto, variable numerica y conversión explicita

---
+++ NumericCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NumericCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA;
+            int numberB;
+
+            Console.WriteLine("Ingrese el primer numero");
+            numberA = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero");
+            numberB = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = numberA * numberB;
+
+            Console.WriteLine($"Resultado => {resultado}");
+        }
+    }
+}
